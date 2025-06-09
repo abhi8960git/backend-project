a09203c483b364a3f02512c31025d02453cba452
Abhishek <abui8994ty@gmail.com>
2025-06-09 16:53:26 +0530

    add utility for error and response

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { DB_NAME } from "./constants";
+import dotenv from 'dotenv'
+import connectDB from "./db";
+
+dotenv.config({
+    path:'./env'
+})
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at ${process.env.PORT}`)
+    }) 
+})
+.catch((err)=>{
+    console.log("MONGO DB CONNECTION FAILED",err);
+})
+// effiy fns
+
+/*
+;(async()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error", (error)=>{
+        console.log("ERROR", error);
+        throw error
+       })
+
+       app.listen(process.env.PORT, ()=>{
+        console.log(`App is running on the port ${process.env.PORT}`);
+       })
+    } catch (error) {
+        console.error("ERROR",error);
+        throw error;
+    }
+})()
+*/
